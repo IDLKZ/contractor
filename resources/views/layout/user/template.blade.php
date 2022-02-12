6e7b2441971ddeaaada60db5e3c0e0583fbf5f21
idlkz <idlkz2019@gmail.com>
2022-02-12 22:39:20 +0600

    1

--- resources/views/layout/user/template.blade.php
+++ resources/views/layout/user/template.blade.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/assets/styles/arrows.css">
     <link rel="stylesheet" href="/assets/styles/style.css">
+    @toastr_css
     @stack("styles")
 
     <title>Kontraktnik.kz - Сервис по приему на воинскую службу по контракту</title>
